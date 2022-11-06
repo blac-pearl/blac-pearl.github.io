60d397299f968f9e22c9ff9f006eb52ec139b217
Chituru Precious <chiturupreciouspearl@gmail.com>
2022-11-06 08:32:13 +0100

    Feat: fixing bugs

--- src/Links.js
+++ src/Links.js
@@ -16,9 +16,9 @@ const Links = () => {
         <a href="https://books.zuri.team/design-rules" target="_blank" rel="noreferrer" id="book__design" className="btn-link">
           <span id="links">Best Design Books Available on Zuri for FREE</span>
         </a>
-        <a href="./Contact" target="_blank" rel="noreferrer" id="contact" className="btn-link">
+        <link to="./Contact" target="_blank" rel="noreferrer" id="contact" className="btn-link">
           <span id="links">Contact Me</span>
-        </a>
+        </link>
         <div id="link-sub">
           <img src="slack.png" alt="slack-logo" />
           <img src="git.png" alt="git-logo" />
